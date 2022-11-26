9d0485ebc53b9a7dc6431ddf2768d5cd792e560a
tkalainoff <t.kalainoff@gmail.com>
2022-11-26 12:16:03 -0500

    creating test data

--- int/asylum_parse.js
+++ int/asylum_parse.js
@@ -1,18 +1,52 @@
 // npm install cheerio
 // npm install n_
+// npm install d3
+
+// import * as d3 from "d3";
 
 var fs = require('fs');
 var cheerio = require('cheerio');
-var _ = require('lodash')
+var _ = require('lodash');
+const d3 = require('d3');
+// var d3 = require('d3');
+// const d3 = await import("d3");
 
 // load helper function library
 var helper = require('./asylum_helpers');
+const internal = require('stream');
 
 // load the cheerio object into a variable, `content`
 // which holds data and metadata about the html file (written as txt)
-let raw = JSON.parse(fs.readFileSync('data/us-asylum-decision-2019.json'));
+let rawData = JSON.parse(fs.readFileSync('data/us-asylum-decision-2019.json'));
 
-console.log(raw)
+// console.log(rawData)
 
-let outcomes = helper.getOutcomes(raw)
-console.log(outcomes)
+let outcomes = helper.getOutcomes(rawData)
+// console.log(outcomes)
+
+let totalOutcomesByStage = d3.rollup(outcomes, v=> d3.sum(v, d => d.stage, d => d.outcome))
+console.log(totalOutcomesByStage)
+
+// desired data structure (total of 3 paths) for https://observablehq.com/@oluckyman/sankey-animation-acyclic-graph
+// path01 {
+//     outcome_rec {
+//         stage_AR: interger,
+//         stage_EO: interger,
+//         stage_IN: integer
+//     }
+//     outcome_pro {
+//         stage_AR: interger,
+//         stage_EO: interger,
+//         stage_IN: integer
+//     }
+//     outcome_rej {
+//         stage_AR: interger,
+//         stage_EO: interger,
+//         stage_IN: integer
+//     }
+//     outcome_clo {
+//         stage_AR: interger,
+//         stage_EO: interger,
+//         stage_IN: integer
+//     }
+// }
